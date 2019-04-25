73031117a760be95e9d98d34663dee8160e77d71
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2019-04-25 17:30:37 +0200

    Enhanced API

--- rules/src/main/resources/calltracer.scala
+++ rules/src/main/resources/calltracer.scala
@@ -4,41 +4,43 @@ package object calltracer {
   sealed trait Tracer { def apply(method: StackTraceElement, args: Any*): Unit }
 
   class Recorder extends Tracer {
-    private[this] var recorded: List[(StackTraceElement, String)] = List.empty
+    private[calltracer] var _recorded: List[(StackTraceElement, String)] = List.empty
+
+    def recorded = _recorded
 
     def apply(method: StackTraceElement, args: Any*): Unit = {
       val sign = signature(method, args: _*)
-      if  (recorded.exists(_._1 == method)) recorded = recorded.map { case (m, _) if m == method => m -> sign case x => x }
-      else recorded :+= method -> sign
-    }
-  
-    def dump(file: File) = {
-      val signatures = recorded.map(_._2).mkString("\n")
-      if (!file.exists) {
-        val writer = new PrintWriter(file)
-        writer.write(signatures)
-        writer.close()
-      }
+      if  (_recorded.exists(_._1 == method)) _recorded = _recorded.map { case (m, _) if m == method => m -> sign case x => x }
+      else _recorded :+= method -> sign
     }
   }
 
   class Visitor extends Tracer {
     lazy val recorded = scala.io.Source.fromFile(signatureFile).getLines.toList
-    private[this] var visited: List[StackTraceElement] = List.empty
+    private[calltracer] var visited: List[StackTraceElement] = List.empty
 
     def apply(method: StackTraceElement, args: Any*): Unit =
       if (recorded contains signature(method, args: _*)) visited :+= method
-
-    sys.addShutdownHook { println(s"Visited stack trace:\n${visited.mkString("\n")}") }
   }
 
   val mode          = "#{mode}"
   val signatureFile = "#{signatureFile}"
-  val trace: Tracer = if (mode == "record") new Recorder else new Visitor
 
-  def dump = trace match {
-    case rec: Recorder => rec.dump(new File(signatureFile))
-    case _ => throw new RuntimeException("The `dump` method is available only in the 'record' mode")
+  val trace   : Tracer   = if (mode == "record") new Recorder else new Visitor
+  def recorder: Recorder = trace.asInstanceOf[Recorder]
+  def visitor : Visitor  = trace.asInstanceOf[Visitor ]
+
+  def dumpRecorded(recorded: List[(StackTraceElement, String)]) = {
+    val signatures = recorded.map(_._2).mkString("\n")
+    println(s"Recorded stack trace:\n${signatures}")
+    val writer = new PrintWriter(new File(signatureFile))
+    writer.write(signatures)
+    writer.close()
+  }
+
+  def dumpVisited = trace match {
+    case rec: Recorder => throw new RuntimeException(s"`dumpVisited` is available only in the 'visit' mode of operation")
+    case vis: Visitor  => println(s"Visited stack trace:\n${vis.visited.mkString("\n")}")
   }
 
   def signature(method: StackTraceElement, args: Any*): String =
