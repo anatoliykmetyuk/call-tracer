f8436485b014f8543476a1308f48d54f96be3ce2
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2019-05-02 10:49:48 +0200

    improvements to the tracing lib

--- rules/src/main/resources/calltracer.scala
+++ rules/src/main/resources/calltracer.scala
@@ -5,7 +5,9 @@ package object calltracer {
   val signatureFile = new File("#{signatureFile}")
   val writer = new PrintWriter(signatureFile)
 
-  def trace(methodName: String, filePath: String, methodPosition: String, args: (String, Any)*) = writer.println(methodName)
+  def trace(methodName: String, filePath: String, methodPosition: String, args: (String, Any)*) = log(methodName)
+
+  def log(msg: String) = writer.println(msg)
 
   def dump() = { println(s"Terminating the printer: ${signatureFile}"); writer.flush(); writer.close() }
 }
